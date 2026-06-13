805993efd82ac25d93539f105599059698fecc07
triet2809 <triet2809@gmail.com>
2026-06-13 10:56:53 +0300

    Intial Spring Boot backend setup

---
+++ src/main/java/com/fpt/sealhackathon/SealHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.sealhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SealHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/sealhackathon/SealHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.sealhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
