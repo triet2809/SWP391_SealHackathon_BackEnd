ea9804141c3ac5a911722e13aa9e47ee49346683
nguyen khang <nguyentienkhang2518@gmail.com>
2026-06-23 19:12:51 +0700

    Add common API response DTO

---
+++ src/main/java/com/fpt/sealhackathon/dto/common/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.fpt.sealhackathon.dto.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
